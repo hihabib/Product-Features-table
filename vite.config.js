2be3d952a5078dc4ca72e84a06b1021e62b17b2e
Habibul Islam <habibulislam6862@gmail.com>
2024-08-28 01:59:25 +0600

    file name changed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'responsive.html'),
         page1: resolve(__dirname, 'desktop.html'), 
         page2: resolve(__dirname, 'mobile.html'),
       }
